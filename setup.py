846d52d60e0b39eb35cb2fe5b9475801fb9a1405
Ben <bengetch@gmail.com>
2019-06-02 21:32:56 -0400

    fix setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name             = 'conclave-data',
+    name             = 'conclave_data',
     version          = '0.0.0.1',
     packages         = find_packages(),
     license          = 'MIT',
-    url              = 'https://github.com/cici-conclave/conclave-data',
+    url              = 'https://github.com/cici-conclave/conclave_data',
     description      = 'Utilities for handling data from remote object stores.',
     long_description = open('README.md').read()
 )
