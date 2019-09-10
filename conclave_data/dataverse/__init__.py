ae8a4c12dc300eb972c835ec1a741a0df386f87f
Ben <bengetch@gmail.com>
2019-09-10 13:14:41 -0400

    update get_data to get policy

--- conclave_data/dataverse/__init__.py
+++ conclave_data/dataverse/__init__.py
@@ -25,7 +25,7 @@ class DataverseData:
         self.cfg = cfg
         self.dataverse_connection = DataverseHandler().get_connection(cfg)
 
-    def get_data(self, file_path):
+    def get_data(self, key, file_path, out_file):
         """
         Retrieve a file from Dataverse.
         """
@@ -36,32 +36,24 @@ class DataverseData:
 
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-        # TODO: implement multiple file retrieval functionality
-        expected_file = self.cfg["source"]["file"]
-        file_found = False
-
         aliased_dv = self.dataverse_connection.get_dataverse(self.cfg["source"]['alias'])
         dataset = aliased_dv.get_dataset_by_doi(self.cfg['source']['doi'])
         files = dataset.get_files()
 
         for f in files:
-            if f.name == expected_file:
-                file_found = True
+            if f.name == key:
                 download_url = f.download_url
-
-                # TODO: handle download failure
                 req = requests.get(download_url, auth=self.dataverse_connection.auth)
 
-                with open("{0}/{1}".format(file_path, f.name), 'wb') as out:
+                with open("{0}/{1}".format(file_path, out_file), 'wb') as out:
                     out.write(req.content)
 
                 print("Wrote object {0} to {1}.".format(f.name, file_path))
-                break
+                return
 
-        if not file_found:
-            print("Could not locate file {}. "
-                  "Check to make sure this file is stored on this Dataverse."
-                  .format(expected_file))
+        print("Could not locate file: {}. "
+            "Check to make sure this file is stored on this Dataverse.\n"
+            .format(expected_file))
 
     def put_data(self, file_path, file):
         """
