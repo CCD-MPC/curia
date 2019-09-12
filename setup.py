2acc773c1253c0546a6d8884ab145a452aaf3d6d
bengetch <bengetch@gmail.com>
2019-09-11 22:41:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name             = 'conclave_data',
+    name             = 'provenance',
     version          = '0.0.0.1',
     packages         = find_packages(),
     license          = 'MIT',
-    url              = 'https://github.com/cici-conclave/conclave_data',
+    url              = 'https://github.com/cici-conclave/provenance',
     description      = 'Utilities for handling data from remote object stores.',
     long_description = open('README.md').read()
 )
